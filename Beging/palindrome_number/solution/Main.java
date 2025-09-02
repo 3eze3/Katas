0e42629707b58638ab0f72521fea35a816a6e5dc
3eze3 <ezekics33@gmail.com>
2025-09-01 22:08:00 -0500

    Add the solution for the palindrome kata

---
+++ Beging/palindrome_number/solution/Main.java
@@ -0,0 +1,9 @@
+package Beging.palindrome_number.solution;
+
+public class Main {
+    public static void main(String[] args) {
+        Resolution rs  = new Resolution();
+        System.out.println(rs.isPalindrome(1001));
+    }
+
+}
